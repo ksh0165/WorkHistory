60104422944f5f9499cc150da0b665bea03cfaf0
ksh0165 <wkssm7575@naver.com>
2020-04-07 15:35:42 +0900

    Create sns_radio_활성.js

---
+++ JavaScript/sns_radio_활성.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>radio 값 가져오기</title>
+    <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+</head>
+  <body>
+    <div style="width:200px;">
+        <input type="radio" name="radio" id="r1" value="1" checked><label for="r1">유튜브</label>
+        <input style="width:200px;" type="text" id="yutTxt" name="yutTxt">
+        <input type="radio" name="radio" id="r2" value="0"><label for="r2">인스타그램</label>
+        <input  style="width:200px;" type="text" id="insTxt" name="insTxt">
+    </div>
+
+    <script type="text/javascript">
+      $(document).ready(function () {
+            // 라디오버튼 클릭시 이벤트 발생
+            $("input:radio[name=radio]").click(function(){
+        
+        if($("input[name=radio]:checked").val() == "1"){
+            $("input:text[name=yutTxt]").attr("disabled",false);
+            $("input:text[name=insTxt]").attr("disabled",true);
+            // radio 버튼의 value 값이 1이라면 활성화
+
+        }else if($("input[name=radio]:checked").val() == "0"){
+            $("input:text[name=yutTxt]").attr("disabled",true);
+            $("input:text[name=insTxt]").attr("disabled",false);
+            // radio 버튼의 value 값이 0이라면 비활성화
+        }
+        });
+      });
+    </script>
+  </body>
+</html>
