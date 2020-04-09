cd804d1e2826d5a406268c30beb510d96429c972
ksh0165 <wkssm7575@naver.com>
2020-04-10 08:57:13 +0900

    Update 제로초_구구단2.js

--- JavaScript/js/제로초_구구단2.js
+++ JavaScript/js/제로초_구구단2.js
@@ -27,12 +27,13 @@
                     result: ''
                 }
             }
+
             render(){
                 return(
                     <div>
                         <div>{this.state.first}곱하기{this.state.second}는?</div>
                         <form>
-                            <input type="number" value={this.state.value}/>
+                            <input type="number" onChange={(e) => this.setState({value:e.target.value})} value={this.state.value}/>
                                 <button>입력!</button>
                         </form>
                         <div>{this.state.result}</div>
