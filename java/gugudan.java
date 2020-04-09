d6a566b4c81a710e77c832c817107259f2394caa
ksh0165 <wkssm7575@naver.com>
2020-04-09 11:15:12 +0900

    Update gugudan.java

--- java/gugudan.java
+++ java/gugudan.java
@@ -4,18 +4,19 @@ public class gugudan
 		int cnt=0;
 		do{
 			for(int i = 1; i < 10; i++){
-				for(int j = 1; j < 10; j++){
+				for(int j = 2; j < 10; j++){
 					System.out.printf("%d * %d = %d\t",j,i,i*j);
 
-					if(j > 5){
+					if(j > 4){
 						break;
-					}
+					}					
 				}
 				System.out.print("\n");
 			}
 		}while(cnt>5);
+			System.out.print("\n");
 			for(int i = 1; i < 10; i++){
-				for(int j = 5; j < 10; j++){
+				for(int j = 6; j < 10; j++){
 					System.out.printf("%d * %d = %d\t",j,i,i*j);
 				}
 				System.out.print("\n");
