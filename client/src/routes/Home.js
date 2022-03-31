a5c7ce293ef06a866ab565f678bc378a7ce9593c
김재성 <wkssm7575@naver.com>
2022-04-01 01:53:08 +0900

    select end

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -2,49 +2,32 @@ import {useEffect, useState} from 'react';
 import { axios } from 'axios';
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
     const [isLoding, setIsLoding] = useState(false);
     const [message,setMessage] = useState("");
     const [maxId,setMaxId] = useState();
-    let maxIdVal = null;
-
+    let newwtDatas = null;
     const getMaxId = async () => {
-        // const data = fetch("http://localhost:3001/notes")
-        // .then(res=>{
-        //     if(res.ok){
-        //         console.log(data);
-        //         {data.notes.map(id=>(
-        //             maxIdVal = id
-        //         ))}
-        //         setMaxId(maxIdVal);
-        //     }
-        // })
         await fetch("http://localhost:3001/notes")
         .then(res=>{
-            return res.json()
-            console.log(res.json);
+            return res.json();
         })
         .then(data =>{
-            console.log(data);
+            setMaxId(data.length+1);
+            console.log(data.length+1);
         })
-        
-            // {response.map(id=>(
-            //     maxIdVal = id
-            // ))}
-            // setMaxId(maxIdVal);
     }
     const getNweets = async () =>{
         await fetch("http://localhost:3001/notes")
         .then(res=>{
-            return res.json()
-            console.log(res.json);
+            return res.json();
         })
         .then(data =>{
-            console.log(data);
+            // newwtDatas= data;
+            data.map((text)=>(
+                setNweets((prev)=>[text,...prev])
+            ));
         })
-        // data2.forEach(element => {
-        //     console.log(element);
-        //     setNweet((prev)=>[element.data(),...prev]);
-        // });
     }
 
     useEffect(()=>{
@@ -62,7 +45,7 @@ const Home = () => {
                 'Content-Type':'application/json'
             },
             body:JSON.stringify({
-                id: maxId+1,
+                id: maxId,
                 text:nweet, 
                 createAt: Date.now()
             })
@@ -86,11 +69,26 @@ const Home = () => {
     }
 
     return (
+        <>
         <form onSubmit={onSubmit}>
             <input type="text" value={nweet} onChange={onChange} placeholder="what`s on your mind?" maxLength={120} />
             <input style={{opacity: isLoding ? 0.3 : 1}} type="submit" value={isLoding ? "트위터 추가중..." : "트위터 추가"} />
             {message}
         </form>
+        <table>
+            <tbody>
+                {nweets.map((nweet)=>(
+                <tr key={nweet.id}>
+                    <td>{nweet.text}</td>
+                    <td>{new Date(nweet.createAt).toLocaleDateString("en-GB",{
+                            hour: "2-digit",
+                            minute: "2-digit"
+                        })}</td>
+                </tr>                   
+                ))}
+            </tbody>
+        </table>
+        </>
     )
 }
 export default Home;
